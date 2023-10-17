20275fb2cdac29e48fc888073248d08da99eba0a
Patryk Dachwitz <patrykda99@gmail.com>
2023-10-17 21:48:43 +0200

    update file Managaer

--- resources/js/admin/components/file_manager/index.vue
+++ resources/js/admin/components/file_manager/index.vue
@@ -9,6 +9,8 @@ import Folder from "@/admin/components/file_manager/folder.vue";
 const url = ref('http://127.0.0.1:8000/api/folders/2');
 const { date, error} = useFetch(url);
 
+provide("urlApiFileManager", url);
+
 setTimeout(() => {
     console.log(date)
     console.log(error)
@@ -17,11 +19,19 @@ setTimeout(() => {
 
 <template>
     <div>
-        <navbar
-            name="menadżer plików"
-        />
-        <div>
+        <div class="bg-dark p-2 ps-3">
+            <div class="fs-4 text-white">
+                Menadżer plików
+            </div>
 
+            <div class="noTypicalBorder fs-4 pt-1">
+                <a class="pe-2"><b>&lt</b></a>
+                <a><b>></b></a>
+                <a class="ps-2"><b>+</b></a>
+            </div>
+        </div>
+        <div class="bg-info sendFile d-flex justify-content-center align-items-center">
+            <div>resr</div>
         </div>
         <div class="row p-4 g-1" @drag="console.log('test')">
             <folder v-for="folder in date.folders" :name="folder.name" />
@@ -33,4 +43,18 @@ setTimeout(() => {
 </template>
 
 <style scoped>
+.sendFile {
+    min-height: 15vh;
+    max-height: 15vh;
+
+}
+.noTypicalBorder a {
+    color: #fff;
+    cursor: pointer;
+
+}
+.noTypicalBorder{
+ border-top: 1px solid #FFF;
+
+}
 </style>
