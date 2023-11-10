5452b8cc8a45968630f1e2cb88ea5b17cb2437ff
Patryk Dachwitz <patrykda99@gmail.com>
2023-11-10 21:35:49 +0100

    update footer

---
+++ lang/pl/footer.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'companyName' => 'BoatsTrip Sp.z.o.o.k',
+    'companyStreet' => 'ul. warszawska 23, Warszawska',
+    'companyCityAndPostalCode' => '64-200 Warszawa',
+    'companyNip' => 'NIP: 123-123-123',
+    'companyPhone' => 'Tel. +48 123 123 123',
+    'companyMail' => 'E-mail: hello@artforma.com',
+    'information' => 'Informacje',
+    'privacyPolicy' => 'Polityka prywatności',
+    'statute' => 'Regulamin',
+    'contact' => 'Kontakt',
+    'clientAccount' => 'Konto klienta',
+    'clientOrders' => 'Zamówienia',
+    'clientLogIn' => 'Logowanie',
+    'clientRegistration' => 'Rejestracja',
+];
--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -1,60 +1,43 @@
-<footer class="footer bg-dark d-flex flex-column text-white shadow">
-    <div class="supportNavigation d-flex justify-content-lg-evenly flex-column flex-lg-row py-4 align-items-center align-items-lg-start w-75">
-        <div class=" d-flex flex-column align-items-center align-items-lg-start">
-            <h3 class="fs-4 mb-3 mb-lg-0" >BoatsTrip Sp.z.o.o.k</h3>
+<footer class="footer bg-dark d-flex flex-column text-white shadow align-items-center ">
+    <div class="supportNavigation d-flex justify-content-lg-center flex-column flex-lg-row py-4 align-items-center align-items-lg-start container-1920">
+        <div class="d-flex flex-column align-items-center align-items-lg-start me-lg-5">
+            <h3 class="fs-4 mb-3 mb-lg-0" >@lang('footer.companyName')</h3>
             <div class="d-flex flex-row flex-lg-column me-5 me-lg-0">
                 <div class=" d-flex flex-column">
 
-                    <span class="fs-6">ul. warszawska 23, Warszawska</span>
-                    <span class="fs-6 ">64-200 Warszawa</span>
+                    <span class="fs-6">@lang('footer.companyStreet')</span>
+                    <span class="fs-6 ">@lang('footer.companyCityAndPostalCode')</span>
                 </div>
                 <hr class="w-50 d-none d-lg-block"/>
                 <div class=" d-flex flex-column ms-5 ms-lg-0">
-                    <span class="fs-6 mb-1">NIP: 123-123-123</span>
+                    <span class="fs-6 mb-1">@lang('footer.companyNip')</span>
 
-                    <span class="fs-6 mb-1">Tel. +48 123 123 123</span>
+                    <span class="fs-6 mb-1">@lang('footer.companyPhone')</span>
 
-                    <span class="fs-6">E-mail: hello@artforma.com</span>
+                    <span class="fs-6">@lang('footer.companyMail')</span>
                 </div>
 
             </div>
             <hr class="w-100 d-block d-lg-none"/>
         </div>
-        <div class="d-flex flex-column">
-            <h3 class="fs-5">Obsługa klienta</h3>
-            <ul class="colorGray list-unstyled fs-6 d-none d-lg-block">
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
+        <div class="d-flex flex-column mx-lg-5">
+            <h3 class="fs-4">@lang('footer.information')</h3>
+            <ul class="colorGray list-unstyled fs-5 d-none d-lg-block">
+                <li><a class="text-decoration-none colorGray" href="{{ route('pages', [ "id" => 2]) }}">@lang('footer.privacyPolicy')</a></li>
+                <li><a class="text-decoration-none colorGray" href="{{ route('pages', [ "id" => 1]) }}">@lang('footer.statute')</a></li>
+                <li><a class="text-decoration-none colorGray" href="{{ route('pages', [ "id" => 3]) }}">@lang('footer.contact')</a></li>
             </ul>
         </div>
-        <div class="d-flex flex-column">
-            <h3 class="fs-5">Obsługa klienta</h3>
-            <ul class="colorGray list-unstyled fs-6 d-none d-lg-block">
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-            </ul>
-        </div>
-        <div class="d-flex flex-column">
-            <h3 class="fs-5">Obsługa klienta</h3>
-            <ul class="colorGray list-unstyled fs-6 d-none d-lg-block">
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
-                <li><a>Regulamin sklepu</a></li>
+        <div class="d-flex flex-column ms-lg-5">
+            <h3 class="fs-4">@lang('footer.clientAccount')</h3>
+            <ul class="colorGray list-unstyled fs-5 d-none d-lg-block">
+                <li><a class="text-decoration-none colorGray" href="{{ route('pages', [ "id" => 2]) }}">@lang('footer.clientOrders')</a></li>
+                <li><a class="text-decoration-none colorGray" href="{{ route('pages', [ "id" => 1]) }}">@lang('footer.clientLogIn')</a></li>
+                <li><a class="text-decoration-none colorGray" href="{{ route('pages', [ "id" => 3]) }}">@lang('footer.clientRegistration')</a></li>
             </ul>
         </div>
     </div>
-    <div class="footer-bottom d-flex flex-column  flex-md-row py-2 fs-6  justify-content-center justify-content-lg-around colorGray">
+    <div class="footer-bottom d-flex flex-column  flex-md-row py-2 fs-6  justify-content-center justify-content-lg-around colorGray w-100">
         <span class="text-center mb-1 mb-lg-0">© Copyright 2023 <b>BoatsTrip</b></span>
         <span class="text-center">created by <b>Patryk Dachwitz</b></span>
     </div>
