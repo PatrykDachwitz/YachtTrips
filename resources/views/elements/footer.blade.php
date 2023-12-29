f941262165e790af8e500580bebf1f24ef837eea
Patryk Dachwitz <patrykda99@gmail.com>
2023-12-29 03:00:38 +0100

    update footer

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -1,5 +1,20 @@
-<footer class="footer bg-dark d-flex flex-column text-white shadow align-items-center ">
-    <div class="supportNavigation d-flex justify-content-lg-center flex-column flex-lg-row py-4 align-items-center align-items-lg-start container-1920">
+<footer class="footer footer-bottom bg-dark text-white shadow d-flex justify-content-center py-1">
+
+    <div class="d-flex flex-column  flex-md-row py-2 fs-6  justify-content-center justify-content-lg-around colorGray w-100" style="max-width: 1920px">
+        <span class="text-center mb-1 mb-lg-0">© Copyright 2023 <b>@lang('menu.nameCompany')</b></span>
+
+        <ul class="colorGray list-unstyled fs-5 d-flex m-0">
+            <li><a class="text-decoration-none colorGray border-end-grayMenu-1 px-2" href="/polityka_prywatnosci">@lang('footer.privacyPolicy')</a></li>
+            <li><a class="text-decoration-none colorGray border-end-grayMenu-1 px-2" href="/regulamin">@lang('footer.statute')</a></li>
+            <li><a class="text-decoration-none colorGray px-2" href="/kontakt">@lang('footer.contact')</a></li>
+        </ul>
+
+        <span class="text-center">created by <b>Patryk Dachwitz</b></span>
+    </div>
+</footer>
+
+<!--
+<div class="supportNavigation d-flex justify-content-lg-center flex-column flex-lg-row py-4 align-items-center align-items-lg-start container-1920 d-none">
         <div class="d-flex flex-column align-items-center align-items-lg-start me-lg-5">
             <h3 class="fs-4 mb-3 mb-lg-0" >@lang('footer.companyName')</h3>
             <div class="d-flex flex-row flex-lg-column me-5 me-lg-0">
@@ -36,9 +51,6 @@
 {{--                <li><a class="text-decoration-none colorGray" href="{{ route('pages', [ "id" => 3]) }}">@lang('footer.clientRegistration')</a></li>--}}
 {{--            </ul>--}}
 {{--        </div>--}}
-    </div>
-    <div class="footer-bottom d-flex flex-column  flex-md-row py-2 fs-6  justify-content-center justify-content-lg-around colorGray w-100">
-        <span class="text-center mb-1 mb-lg-0">© Copyright 2023 <b>BoatsTrip</b></span>
-        <span class="text-center">created by <b>Patryk Dachwitz</b></span>
-    </div>
-</footer>
+</div>
+
+-->
